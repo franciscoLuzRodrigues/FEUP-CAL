498a5e43bc2c84087f2748e29cef2d5be2c5a5c9
mafaldasantos10 <anamaf13@gmail.com>
2019-05-13 16:01:42 +0100

    Added graph viewer files

--- schoolBus.cpp
+++ schoolBus.cpp
@@ -57,8 +57,9 @@ int School::getY()
 	return y;
 }
 
-void School::getY(int y)
+void School::setY(int y)
 {
+	this->y = y;
 }
 
 
@@ -71,12 +72,62 @@ Student::Student(string name, int age, School* school)
 	this->school = school;
 }
 
+string Student::getName()
+{
+	return name;
+}
+
+void Student::setName(string name)
+{
+	this->name = name;
+}
+
+int Student::getAge()
+{
+	return age;
+}
+
+void Student::setAge(int age)
+{
+	this->age = age;
+}
+
+School* Student::getSchool()
+{
+	return school;
+}
+
+void Student::setSchool(School* school)
+{
+	this->school = school;
+}
+
 ///////////////BUSSTOP////////////////////////////////
 
 BusStop::BusStop(int x, int y, vector<Student*> studentsInStop)
 {
 	this->studentsInStop = studentsInStop;
 	this->x = x;
+	this->y = y;
+}
+vector<Student*> BusStop::getStudentsInStop()
+{
+	return studentsInStop;
+}
+int BusStop::getX()
+{
+	return x;
+}
+void BusStop::setX(int x)
+{
+	this->x = x;
+}
+int BusStop::getY()
+{
+	return y;
+}
+void BusStop::setY(int y)
+{
 	this->y = y;
 }
 //////////////BUS/////////////////////////////////////
@@ -90,6 +141,46 @@ Bus::Bus(int busId, School* school, vector<Student*> busStudents, bool apt, int 
 	this->capacity = capacity;
 	this->occupation = occupation;
 }
+int Bus::getBusId()
+{
+	return busId;
+}
+void Bus::setBusId()
+{
+	this->busId = busId;
+}
+School * Bus::getSchool()
+{
+	return school;
+}
+void Bus::setSchool(School * school)
+{
+	this->school = school;
+}
+bool Bus::getApt()
+{
+	return apt;
+}
+void Bus::setApt(bool apt)
+{
+	this->apt = apt;
+}
+int Bus::getCapacity()
+{
+	return capacity;
+}
+void Bus::setCapacity(int capacity)
+{
+	this->capacity = capacity;
+}
+int Bus::getOccupation()
+{
+	return occupation;
+}
+void Bus::setOccupation(int occupation)
+{
+	this->occupation = occupation;
+}
 //////////////GARAGE//////////////////////////////////
 
 Garage::Garage(int numberOfBuses, vector<Bus*> busesInGarage, vector<Bus*> busesToGo, int x, int y)
@@ -98,5 +189,35 @@ Garage::Garage(int numberOfBuses, vector<Bus*> busesInGarage, vector<Bus*> buses
 	this->busesInGarage = busesInGarage;
 	this->busesToGo = busesToGo;
 	this->x = x;
+	this->y = y;
+}
+
+vector<Bus*> Garage::getBusesInGarage()
+{
+	return busesInGarage;
+}
+
+vector<Bus*> Garage::getBusesToGo()
+{
+	return busesToGo;
+}
+
+int Garage::getX()
+{
+	return x;
+}
+
+void Garage::setX(int x)
+{
+	this->x = x;
+}
+
+int Garage::getY()
+{
+	return y;
+}
+
+void Garage::setY(int y)
+{
 	this->y = y;
 }
