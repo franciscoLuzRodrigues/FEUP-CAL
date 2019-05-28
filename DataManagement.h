09ef220e9a17ad8e891672d5966f61bb54ee801f
mafaldasantos10 <anamaf13@gmail.com>
2019-05-28 17:21:48 +0100

    documentation

--- DataManagement.h
+++ DataManagement.h
@@ -27,34 +27,218 @@ private:
 
 
 public:
+	/**
+ 	* @brief Graph tthat stores the map information loaded from the files
+ 	* 
+ 	*/
 	Graph<Node> graph;
+	
+	/**
+	 * @brief Construct a new Data Manager object
+	 * 
+	 */
 	DataManager(){};
+
+	/**
+	 * @brief Reads Vertex information
+	 * 
+	 * @param fileName Name of the file to be read
+	 * @return int - 0 if successfull and 1 if not
+	 */
 	int readNodeFile(string fileName);
+
+	/**
+	 * @brief Reads Edge information
+	 * 
+	 * @param fileName Name of the file to be read
+	 * @return int - 0 if successfull and 1 if not
+	 */
 	int readEdgeFile(string fileName);
+
+	/**
+	 * @brief Reads information about points of interest such as schools and bus stops
+	 * 
+	 * @param fileName Name of the file to be read
+	 * @return int - 0 if successfull and 1 if not
+	 */
 	int loadPointsOfInterest(string poiFilePath);
+	
+	/**
+	 * @brief Adds school to the schools vector
+	 * 
+	 * @param school new school to be added
+	 */
 	void addSchool(School school);
+
+	/**
+	 * @brief Adds a student to the student vector
+	 * 
+	 * @param student new Student to be added
+	 */
 	void addStudent(Student student);
+
+	/**
+	 * @brief Adds a new busStop to the vector
+	 * 
+	 * @param busStop new BusStop to be added
+	 */
 	void addBusStop(BusStop busStop);
+
+	/**
+	 * @brief  Removes the student at position index of vector Students
+	 * 
+	 * @param index of the students vector
+	 */
 	void eraseStudent(int index) { students.erase(students.begin() + index); }
+
+	/**
+	 * @brief Removes the busStop at position index of vector busStops
+	 * 
+	 * @param index of the busStop vector
+	 */
 	void eraseBusStop(int index) { busStops.erase(busStops.begin() + index); }
+
+	/**
+	 * @brief Removes a bus from the garage vector of buses
+	 * 
+	 * @param index of the garage bus vector
+	 */
 	void eraseBus(int index) { garage.eraseBus(index); }
+
+	/**
+	 * @brief Get the Schools object
+	 * 
+	 * @return vector<School> all possible schools
+	 */
 	vector<School> getSchools() { return schools; }
+
+	/**
+	 * @brief Get the All Bus Stops vector
+	 * 
+	 * @return vector<BusStop>* all possible busStops
+	 */
 	vector<BusStop>* getAllBusStops() { return &allBusStops; }
+
+	/**
+	 * @brief Adds a bus stop to the AllBusStops vector
+	 * 
+	 * @param busStop new busStop
+	 */
 	void addAllBusStop(BusStop busStop);
+
+	/**
+	 * @brief Gets the vector with the busStops where the bus will have to pass
+	 * 
+	 * @return vector<BusStop>* busStopsVector
+	 */
 	vector<BusStop>* getBusStops() { return &busStops; }
+
+	/**
+	 * @brief Get the Students vector
+	 * 
+	 * @return vector<Student> vector with all the students
+	 */
 	vector<Student> getStudents() { return students; }
+
+	//FRANCISCO
+
+	/**
+	 * @brief Generates a vector with the bus stops ordered by the position the bus has to pass them
+	 * 
+	 * @param bStops vector to be ordered
+	 * @return vector<Node> with the nodes corresponding to each bus stop in order
+	 */
 	vector<Node> getPathNodes(vector<Node> &bStops);
+
+	/**
+	 * @brief Calculates each bus's path (using the njearest points to the garage)
+	 * 
+	 * @return vector<vector<Node>> vector of vectors where each vector represents a bus's path
+	 */
 	vector<vector<Node>> getMultPaths();
+
+	/**
+	 * @brief Get the path for each bus, generating a path using a vector of nodes 
+	 * 
+	 * @return vector<vector<Node>> vector where each index contains a complete path of a bus
+	 */
 	vector<vector<Node>> getPath(); 
+	
+	/**
+	 * @brief Set the School object
+	 * 
+	 * @param school new school
+	 */
 	void setSchool(School school) { this->school = school; }
+	
+	/**
+	 * @brief Set the Garage object
+	 * 
+	 * @param garage new garage
+	 */
 	void setGarage(Garage garage) { this->garage = garage; }
+
+	/**
+	 * @brief Get the Garage object
+	 * 
+	 * @return Garage the current garage
+	 */
 	Garage getGarage() { return garage; }
+	
+	/**
+	 * @brief Adds a bus to a garage so it can be used for transport
+	 * 
+	 * @param bus  to be added
+	 */
 	void addBusToGarage(Bus bus){garage.addBus(bus);}
+
+	/**
+	 * @brief Construct a new vector<Node> based on the busStops b
+	 * 
+	 * @param b Vector of busStops
+	 * 
+	 * @return vector<Node> vector with the nodes that correspond to the b busStops
+	 */
 	vector<Node>toVecNode(vector<BusStop> b);
+	
+	/**
+	 * @brief Erases bus stops based on their ID
+	 * 
+	 * @param bStops vector where it is intended to delete
+	 * @param id of the busStop to be deleted
+	 */
 	void eraseNodeWithID(vector<Node>& bStops, int id);
+
+	/**
+	 * @brief Find and returns the busStop with the vertex Node
+	 * 
+	 * @param temp vector of busStops
+	 * @param node vertex to be foung
+	 * @return BusStop* with vertex Node
+	 */
 	BusStop* getBusStopByNode(vector<BusStop> &temp, Vertex<Node> *node);
+
+	/**
+	 * @brief Adds a student a bus stop
+	 * 
+	 * @param student to be added to the bus stop
+	 * @param index of the bus stop to wich the student will be added
+	 */
 	void increaseStudent(Student *student, int index);
+
+	/**
+	 * @brief Empties the queue and reinsertes all nodes in the queue in order to update it
+	 * 
+	 * @param newBusStop nodes to be added to the queue
+	 */
 	void updateQ(vector<Node> newBusStop);
+
+	/**
+	 * @brief Verifies if the number of students to be transported is less then the total capacity of all buses
+	 * 
+	 * @return true if the number of students to be transported is less then the total capacity of all buses
+	 * @return false otherwise
+	 */
 	bool verifyNumberStudents();
 
 };
--- SchoolBus.h
+++ SchoolBus.h
@@ -12,25 +12,77 @@
 
 using namespace std;
 
+
 class Node {
+	
+	/**@brief Node ID;  */
 	int id;
+	
+	/**@brief X coordinate;  */
 	float x;
+	
+	/**@brief Y coordinate;  */
 	float y;
-	//int value;
 
 	public:
+	/**
+	 * @brief Construct a new Node object
+	 * 
+	 * @param id Node id
+	 * @param x X coordinate
+	 * @param y Y coordinate
+	 */
 	Node(int id, float x, float y) { this->id = id; this->x = x; this->y = y; }
+
+	/**
+	 * @brief Gets node ID
+	 * 
+	 * @return int ID Node id
+	 */
 	int getID(){ return id;}
+
+	/**
+	 * @brief Gets node X coordinate
+	 * 
+	 * @return float Node's X coordinate
+	 */
 	float getX(){return x;}
+
+	/**
+	 * @brief Gets node Y coordinate
+	 * 
+	 * @return float Node's Y coordinate
+	 */
 	float getY(){return y;}
-	//int getValue(){return value;}
 
+	/**
+	 * @brief Sets the Id
+	 * 
+	 * @param id new id
+	 */
 	void setId(int id){this->id = id;}
-	void setX(float lat){this->x = lat;}
-	void setY(float lon){this->y = lon;}
-	//void setValue(int value){this->value = value;}
 
+	/**
+	 * @brief Sets the X coordinate
+	 * 
+	 * @param x new X 
+	 */
+	void setX(float x){this->x = x;}
 
+	/**
+	 * @brief Sets the X coordinate
+	 * 
+	 * @param y 
+	 */
+	void setY(float y){this->y = y;}
+
+	/**
+	 * @brief operator used to compare nodes
+	 * 
+	 * @param other node to be compared
+	 * @return true if nodes are the same
+	 * @return false if nodes are not the same
+	 */
 	bool operator == (Node other) {
 		if (this->id == other.getID())
 			if (this->x == other.getX())
@@ -39,12 +91,23 @@ class Node {
 		return false;
 	}
 
+	/**
+	 * @brief operator used to copy data from one Node to another
+	 * 
+	 * @param other node to be copied
+	 */
 	void operator = (Node other) {
 		this->id = other.getID();
 		this->x = other.getX();
 		this->y = other.getY();
 	}
 
+	/**
+	 * @brief Get the distance between this node and another
+	 * 
+	 * @param other node at unkown distance
+	 * @return double distance between the nodes
+	 */
 	double getDistance(Node other) {
 		return sqrt(pow(this->x - other.getX(), 2) + pow(this->y - other.getY(), 2));
 	}
@@ -53,35 +116,150 @@ class Node {
 
 
 class School {
+	
+	/**@brief school's name;  */
 	string name;
+
+	/**@brief time at which school opens;  */
 	int morningTime;
+
+	/**@brief time at which school closes;  */
 	int afternoonTime;
+
+	/**@brief Vertex that defines school coordinates;  */
 	Vertex<Node> *node;
 
 public:
+	/**
+	 * @brief Construct a new School object
+	 * 
+	 */
 	School(){}
+	/**
+	 * @brief Construct a new School object
+	 * 
+	 * @param name of the school
+	 * @param morningTime time at which school opens
+	 * @param afternoonTime time at which school closes
+	 * @param node Vertex that defines school coordinates
+	 */
 	School(string name, int morningTime, int afternoonTime, Vertex<Node> *node);
+
+	/**
+	 * @brief Gets name of the school
+	 * 
+	 * @return string name of school
+	 */
 	string getName();
+
+	/**
+	 * @brief Set name of the school
+	 * 
+	 * @param name new name
+	 */
 	void setName(string name);
+
+	/**
+	 * @brief Get Morning Time
+	 * 
+	 * @return int morning time
+	 */
 	int getMorningTime();
+
+	/**
+	 * @brief Set Morning Time 
+	 * 
+	 * @param time new morning time
+	 */
 	void setMorningTime(int time);
+
+	/**
+	 * @brief Get Afternoon Time
+	 * 
+	 * @return int afternoon time
+	 */
 	int getAfternoonTime();
+
+	/**
+	 * @brief Set the Afternoon Time object
+	 * 
+	 * @param time new afternoon time
+	 */
 	void setAfternoonTime(int time);
+
+	/**
+	 * @brief Set the Vertex that contains the school's node
+	 * 
+	 * @param node new node
+	 */
 	void setNode(Vertex<Node>* node) { this->node = node; }
+
+	/**
+	 * @brief Get school's vertex
+	 * 
+	 * @return Vertex<Node>* node
+	 */
 	Vertex<Node>* getNode() { return node; }
 };
 
 class Student {
+
+	/**@brief Student name;  */
 	string name;
+
+	/**@brief Student age;  */
 	int age;
 
 public:
+	/**
+	 * @brief Construct a new Student object
+	 * 
+	 * @param name of the student
+	 * @param age of the student
+	 */
 	Student(string name, int age);
+
+	/**
+	 * @brief Get student's name
+	 * 
+	 * @return string student's name
+	 */
 	string getName();
+
+	/**
+	 * @brief Set student's name
+	 * 
+	 * @param name new student's name
+	 */
 	void setName(string name);
+
+	/**
+	 * @brief Get student's age
+	 * 
+	 * @return int student's age
+	 */
 	int getAge();
+
+	/**
+	 * @brief Set student's age
+	 * 
+	 * @param age new student's age
+	 */
 	void setAge(int age);
+
+	/**
+	 * @brief Get student's information
+	 * 
+	 */
 	void getInfo();
+
+	/**
+	 * @brief operator used to compare students
+	 * 
+	 * @param other student to be compared
+	 * @return true if students are the same
+	 * @return false if students are not the same
+	 */
 	bool operator ==(Student other){
 		if(this->name == other.getName() && this->age == other.getAge())
 				return true;
@@ -90,50 +268,215 @@ public:
 };
 
 class BusStop {
+
+	/**@brief vector of students in the busStop;  */
 	vector<Student*> studentsInStop;
+	
+	/**@brief busStop address;  */
 	string address;
+	
+	/**@brief node where it's located;  */
 	Vertex<Node> *node;
 
 public:
+	/**
+	 * @brief Construct a new Bus Stop object
+	 * 
+	 * @param node Vertex that contains Bus Stop location
+	 * @param address of the Bus Stop
+	 */
 	BusStop(Vertex<Node> *node,string address);
+
+	/**
+	 * @brief Get pointer to vector containing the students at this bus stop
+	 * 
+	 * @return vector<Student*>* pointer to vector containing the students
+	 */
 	vector<Student*>* getStudentsInStop();
+
+	/**
+	 * @brief Adds a student to the Bus Stop
+	 * 
+	 * @param student to be added
+	 */
 	void addStudentInStop(Student* student){studentsInStop.push_back(student);}
+
+	/**
+	 * @brief Erases a student from the Bus Stop
+	 * 
+	 * @param index of the student in studentsInStop vector
+	 */
 	void eraseStudentInStop(int index){studentsInStop.erase(studentsInStop.begin() + index);}
+
+	/**
+	 * @brief Get the Address of the bus stop
+	 * 
+	 * @return string addrees
+	 */
 	string getAddress(){return address;}
+
+	/**
+	 * @brief Set the Address of the bus stop
+	 * 
+	 * @param address new bus stop address
+	 */
 	void setAddress(string address){this->address = address;}
+
+	/**
+	 * @brief Set the Node(Vertex<Node>) of the bus stop 
+	 * 
+	 * @param node new Vertex<Node>
+	 */
 	void setNode(Vertex<Node> *node) { this->node = node; }
+	
+	/**
+	 * @brief Get the Node(Vertex<Node>) of the bus stop
+	 * 
+	 * @return Vertex<Node>* Vertex
+	 */
 	Vertex<Node> * getNode() { return node; }
 };
 
 class Bus {
+
+	/**@brief true if the bus isn't apt;  */
 	bool apt;
+	
+	/**@brief bus capatity;  */
 	int capacity;
+	
+	/**@brief number of students in the bus;  */
 	int occupation;
 
 public:
+
+	/**
+	 * @brief Construct a new Bus object
+	 * 
+	 * @param apt - checks if bus can transport
+	 * @param capacity - bus's capacity
+	 * @param occupation - bus's curret occupation
+	 */
 	Bus(bool apt, int capacity, int occupation);
+	
+	/**
+	 * @brief Get the bus's destination
+	 * 
+	 * @return School* - pointer to a School object that corresponds to the bus destination
+	 */
 	School* getSchool();
+
+	/**
+	 * @brief Set the bus's destination
+	 * 
+	 * @param school - pointer to a School object that corresponds to the bus destination
+	 */
 	void setSchool(School* school);
+
+	/**
+	 * @brief Get the Apt object
+	 * 
+	 * @return true if bus is able to transport (is not broken)
+	 * @return false otherwise
+	 */
 	bool getApt();
+
+	/**
+	 * @brief Set the Apt object
+	 * 
+	 * @param apt 
+	 */
 	void setApt(bool apt);
+
+	/**
+	 * @brief Get the bus Capacity
+	 * 
+	 * @return int 
+	 */
 	int getCapacity();
+
+	/**
+	 * @brief Set the bus Capacity
+	 * 
+	 * @param capacity 
+	 */
 	void setCapacity(int capacity);
+	
+	/**
+	 * @brief Get the bus Occupation
+	 * 
+	 * @return amount of students in the bus
+	 */
 	int getOccupation();
+
+	/**
+	 * @brief Set the bus Occupation
+	 * 
+	 * @param new occupation value 
+	 */
 	void setOccupation(int occupation);
+
+	/**
+	 * @brief Displays the bus information
+	 * 
+	 */
 	void getInfo();
 };
 
 class Garage {
+
+	/**@brief number of buses in the bus;  */
 	int numberOfBuses;
+
+	/**@brief vector with the vehicles;  */
 	vector<Bus> companyVehicles;
+
+	/**@brief node where the garage is located;  */
 	Vertex<Node> *node;
 
 public:
+
+	/**
+	 * @brief Construct a new Garage object
+	 * 
+	 * @param busesInGarage vector with all the buses in the garage
+	 * @param numberOfBuses number of buses in the garage
+	 * @param node Node where the garage is located
+	 */
 	Garage(vector<Bus> busesInGarage, int numberOfBuses, Vertex<Node> *node);
+
+	/**
+	 * @brief Construct a new Garage object
+	 * 
+	 */
 	Garage(){}
+
+	/**
+	 * @brief Get the Buses vector
+	 * 
+	 * @return vector<Bus > vector of buses
+	 */
 	vector<Bus > getBuses(){return companyVehicles;}
+
+	/**
+	 * @brief Get the vertex object
+	 * 
+	 * @return Vertex<Node>* vertex
+	 */
 	Vertex<Node> *getNode(){return node;}
+
+	/**
+	 * @brief Adds a new bus to the garage
+	 * 
+	 * @param bus new bus to be added
+	 */
 	void addBus(Bus bus){companyVehicles.push_back(bus);}
+
+	/**
+	 * @brief removed a bus from the buses vector
+	 * 
+	 * @param index of the bus to be removed
+	 */
 	void eraseBus(int index){companyVehicles.erase(companyVehicles.begin() + index);}
 };
 
